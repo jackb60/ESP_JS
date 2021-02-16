8fe59f557842d4724de26a5d54e5baa2e9876458
jackb60 <40903548+jackb60@users.noreply.github.com>
2021-02-16 10:53:11 -0500

    Update led.js

--- led.js
+++ led.js
@@ -1,8 +1,8 @@
 update_raw = function(){
-  a=document.getElementById('pxl');
-  b=document.getElementById('rd');
-  c=document.getElementById('gr');
-  d=document.getElementById('bl');
+  a=document.getElementById('pxl').value;
+  b=document.getElementById('rd').value;
+  c=document.getElementById('gr').value;
+  d=document.getElementById('bl').value;
   bffr=new Uint8Array([a,b,c,d]).buffer;
   fetch('/LED',fetch('/LED',{method:'POST',body:bffr}))
 }
